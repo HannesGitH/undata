f3c3009509c96c369c69a1037c3537de3c6c177e
HannesH <33062605+HannesGitH@users.noreply.github.com>
2023-01-05 14:04:49 +0100

    vllt so

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import GithubActionsReporter from 'vitest-github-actions-reporter';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
+		reporters: process.env.GITHUB_ACTIONS ? ['default', new GithubActionsReporter()] : 'default',
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
 };
